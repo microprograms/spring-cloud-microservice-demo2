e8221c13f12320b83798cc554571861f05e7f0a2
xuzewei <microprograms@126.com>
2021-02-26 15:41:27 +0800

    openfeign

--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/Demo2Controller.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/Demo2Controller.java
@@ -3,18 +3,16 @@ package com.github.microprograms.spring_cloud_microservice_demo2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class Demo2Controller {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private Demo1Feign demo1Feign;
 
     @RequestMapping("/health")
     public String health() {
-        String demo1HealthResponse = restTemplate.getForObject("http://spring-cloud-microservice-demo1/health",
-                String.class);
+        String demo1HealthResponse = demo1Feign.health();
         if ("Ok".equals(demo1HealthResponse)) {
             return "spring-cloud-microservice-demo1 is Ok and spring-cloud-microservice-demo2 is Ok";
         } else {
