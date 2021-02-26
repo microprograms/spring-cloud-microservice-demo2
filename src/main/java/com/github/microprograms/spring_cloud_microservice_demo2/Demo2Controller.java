4b1d81f4bf014ac898f83a396359edb77b21e8a7
xuzewei <microprograms@126.com>
2021-02-26 12:03:00 +0800

    application.yml

--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/Demo2Controller.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/Demo2Controller.java
@@ -1,22 +1,25 @@
 package com.github.microprograms.spring_cloud_microservice_demo2;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
-@RequestMapping("/demo2")
+@RestController
 public class Demo2Controller {
 
-    @Value("${spring.application.name}")
-    private String applicationName;
+    @Autowired
+    private RestTemplate restTemplate;
 
-    @GetMapping("/health")
-    @ResponseBody
+    @RequestMapping("/health")
     public String health() {
-        return applicationName + " is Ok";
+        String demo1HealthResponse = restTemplate.getForObject("http://spring-cloud-microservice-demo1/health",
+                String.class);
+        if ("Ok".equals(demo1HealthResponse)) {
+            return "spring-cloud-microservice-demo1 is Ok and spring-cloud-microservice-demo2 is Ok";
+        } else {
+            return "spring-cloud-microservice-demo2 is Ok";
+        }
     }
 
 }
--- src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/SpringCloudMicroserviceDemo2Application.java
+++ src/main/java/com/github/microprograms/spring_cloud_microservice_demo2/SpringCloudMicroserviceDemo2Application.java
@@ -1,13 +1,29 @@
 package com.github.microprograms.spring_cloud_microservice_demo2;
 
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudMicroserviceDemo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudMicroserviceDemo2Application.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+		return restTemplate;
 	}
 
 }
